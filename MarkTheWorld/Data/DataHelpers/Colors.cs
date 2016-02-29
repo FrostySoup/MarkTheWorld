be37f5dcb98e63bcddec8f3be99cece485e809ac
Giedrius Verbyla <giedrius3600@gmail.com>
2016-02-29 23:28:46 +0200

    wth

---
+++ MarkTheWorld/Data/DataHelpers/Colors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.DataHelpers
+{
+    public class Colors
+    {
+        public int Red { get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+    }
+}
---
+++ MarkTheWorld/Data/StringValueAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Data
+{
+    public class StringValue : System.Attribute
+    {
+        private string _value;
+
+        public StringValue(string value)
+        {
+            _value = value;
+        }
+
+        public string Value
+        {
+            get { return _value; }
+        }
+
+    }
+}
