bd5ce1cd9b68170e3e5bb5f0d6b87e81eb3b6583
Andrius Rimkus <rimkusrimkus@gmail.com>
2016-07-04 14:30:35 +0300

    Claim spot

--- MarkTheWorld/MarkTheWorld/Scripts/claimSpot/claimSpotController.js
+++ MarkTheWorld/MarkTheWorld/Scripts/claimSpot/claimSpotController.js
@@ -2,12 +2,18 @@
 (function () {
     'use strict';
 
-    function claimSpotController($mdDialog) {
+    function claimSpotController($mdDialog, Upload) {
         var vm = this;
+        vm.file = null;
 
         vm.cancel = function () {
             $mdDialog.cancel();
         };
+
+        vm.cancelImageUpload = function () {
+            vm.file = null;
+        };
+
 
         //$scope.confirm = function (message) {
         //    accountService.addPoint(message, mapService.getClickedPosition().lat, mapService.getClickedPosition().lng)
