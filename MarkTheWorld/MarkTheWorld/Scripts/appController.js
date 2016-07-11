c72e34e76b6c074e7e8bcbfc484e9c3c163a2f43
Andrius Rimkus <rimkusrimkus@gmail.com>
2016-07-11 16:53:40 +0300

    Bottom controls

--- MarkTheWorld/MarkTheWorld/Scripts/appController.js
+++ MarkTheWorld/MarkTheWorld/Scripts/appController.js
@@ -2,11 +2,19 @@
 (function () {
     'use strict';
 
-    function AppController($scope, accountService, claimSpotService) {
+    function AppController($scope, accountService, claimSpotService, userService) {
         accountService.appStartUpLoginCheck();
         $scope.temp = function (ev) {
             claimSpotService.showDialog(ev);
         };
+
+        $scope.$watch(function () {
+            return userService.currentPosition;
+        }, function (newVal, oldVal) {
+            if (newVal !== null) {
+
+            }
+        });
     }
 
     angular.module('markTheWorld').controller('AppController', AppController);
